b5f6ff5c807d7ef22d2db5f32b4c57949a84e461
@racinefell <102471647+racinefe@users.noreply.github.com>
2024-07-12 17:27:53 -0300

    adicionando progress bar a area de conhecimento

--- src/app/modules/portfolio/components/knowledge/knowledge.component.ts
+++ src/app/modules/portfolio/components/knowledge/knowledge.component.ts
@@ -1,41 +1,47 @@
 import { Component, signal } from '@angular/core';
-
-//interface
+// Interface
 import { Iknowledge } from '../../interface/iknowledge.interface';
+// Angular Material
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-knowledge',
   standalone: true,
-  imports: [],
+  imports: [MatProgressBarModule],
   templateUrl: './knowledge.component.html',
-  styleUrl: './knowledge.component.scss'
+  styleUrls: ['./knowledge.component.scss']
 })
 export class KnowledgeComponent {
   public arrayKnowledge = signal<Iknowledge[]>([
     {
       src: 'assets/icons/knowledge/html5.svg',
-      alt: 'Ícone do html5'
+      alt: 'Ícone do html5',
+      progress: 80
     },
     {
       src: 'assets/icons/knowledge/sass.svg',
-      alt: 'Ícone do sass'
+      alt: 'Ícone do sass',
+      progress: 70
     },
     {
       src: 'assets/icons/knowledge/javascript.svg',
-      alt: 'Ícone do JavaScript'
+      alt: 'Ícone do JavaScript',
+      progress: 80
     },
     {
       src: 'assets/icons/knowledge/react.svg',
-      alt: 'Ícone do React'
+      alt: 'Ícone do React',
+      progress: 35
     },
     {
       src: 'assets/icons/knowledge/angular.svg',
-      alt: 'Ícone do Angular'
+      alt: 'Ícone do Angular',
+      progress: 75
     },
     {
       src: 'assets/icons/knowledge/java.svg',
-      alt: 'Ícone do Java'
-    },
-
-  ])
+      alt: 'Ícone do Java',
+      progress: 35
+    }
+  ]);
 }
--- src/app/modules/portfolio/interface/iknowledge.interface.ts
+++ src/app/modules/portfolio/interface/iknowledge.interface.ts
@@ -1,4 +1,5 @@
 export interface Iknowledge{
     src: string;
     alt:string;
+    progress: number;
 }
