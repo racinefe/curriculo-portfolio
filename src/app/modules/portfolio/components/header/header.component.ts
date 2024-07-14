1e45b84e2a68a6542b9d28c15242bc3d3beed39f
@racinefell <102471647+racinefe@users.noreply.github.com>
2024-07-14 10:47:57 -0300

    alterando imagem de perfil e adicionando animação ao titulo principal

--- src/app/modules/portfolio/components/header/header.component.ts
+++ src/app/modules/portfolio/components/header/header.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    this.startTypingAnimation();
+  }
 
+  startTypingAnimation() {
+    const nameElement = document.querySelector<HTMLElement>('.animated-name');
+    
+    if (nameElement) {
+      const fullName = 'Racine Fellipe';
+      const originalText = fullName.split(' ')[1] + ' </>'; // Acrescentar texto depois de 'Fellipe'
+      nameElement.textContent = fullName.split(' ')[0] + ' '; // Acrescentar texto depois de 'Racine '
+
+      let currentIndex = 0;
+
+      const type = () => {
+        if (currentIndex < originalText.length) {
+          nameElement.textContent = (nameElement.textContent ?? '') + originalText.charAt(currentIndex);
+          currentIndex++;
+          setTimeout(type, 200);
+        } else {
+          setTimeout(erase, 1200);
+        }
+      };
+
+      const erase = () => {
+        if (currentIndex > 0) {
+          nameElement.textContent = (nameElement.textContent ?? '').slice(0, -1);
+          currentIndex--;
+          setTimeout(erase, 200);
+        } else {
+          setTimeout(type, 1200);
+        }
+      };
+
+      // Começa a animação de digitação
+      type();
+    }
+  }
 }
