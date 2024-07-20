48ef3c33d6923d78fe81e64b6fc4ee6715277b4a
@racinefell <102471647+racinefe@users.noreply.github.com>
2024-07-20 18:10:47 -0300

    adicionando efeito zoom e imagem nos cards de projetos

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -22,12 +22,14 @@ export class ProjectsComponent {
     {
       src: 'assets/img/projects/ProjetoCadastro.png',
       alt: 'Projeto Sistema de Matrícula',
-      title: 'Sistema de Matrícula',
+      title: `Sistema de Matrícula`,
       with: '120px',
       height:'91px',
-      description: `<p>Projeto Full Stack de matrícula, utilizando o Firebase como banco de dados para armazenamento,
+      description: `<img src="assets/img/projects/ProjetoCadastro.png" class="img-custom-size" >
+                    <p>Projeto Full Stack de matrícula, utilizando o Firebase como banco de dados para armazenamento,
                        o projeto possui todas as funcionalidades de um GRUD (create, read, update e delete).
                     </p>
+                    
                     <p>Tecnologias utilizadas - HTML| CSS |JavaScript | React.js | Vite | Node.js</p>
                    `,
       links: [
@@ -43,7 +45,8 @@ export class ProjectsComponent {
       title: 'Lista de Tarefas',
       with: '120px',
       height:'91px',
-      description: `<p>Projeto web, onde criei um to-do list, o projeto possui todas as funcionalidades do GRUD (create, read, update e delete),
+      description: `<img src="assets/img/projects/ProjetoToDoList.png" class="img-custom-size" >
+                    <p>Projeto web, onde criei um to-do list, o projeto possui todas as funcionalidades do GRUD (create, read, update e delete),
                        além de um checklist interativo.
                     </p>
                     <p>Tecnologias utilizadas - HTML| CSS |JavaScript </p>
@@ -62,7 +65,8 @@ export class ProjectsComponent {
       with: '120px',
       height:'91px',
       
-      description: `<p>Projeto web, onde cumpri o desafio de construir um formulário de boletim informativo e fazê-lo parecer o mais próximo possível do design.</p>
+      description: `<img src="assets/img/projects/ProjetoMentor.png" class="img-custom-size" >
+                    <p>Projeto web, onde cumpri o desafio de construir um formulário de boletim informativo e fazê-lo parecer o mais próximo possível do design.</p>
                     <p>O desafio foi retirado do Frontend Mentor: <a href="https://www.frontendmentor.io/challenges/newsletter-signup-form-with-success-message-3FC1AZbNrv" target="_blank" class="btn btn-primary">Pagina do desafio</a></p>
                     <p>Tecnologias utilizadas - HTML| CSS |JavaScript | React | vite</p>
                    `,
@@ -82,7 +86,8 @@ export class ProjectsComponent {
       with: '120px',
       height:'91px',
       
-      description: `<p>Projeto web, onde criei um Pokédex interativo sendo possível pesquisar através do nome de um Pokémon ou do ID.
+      description: `<img src="assets/img/projects/ProjetoPokedex.png" class="img-custom-size" >
+                    <p>Projeto web, onde criei um Pokédex interativo sendo possível pesquisar através do nome de um Pokémon ou do ID.
                        O projeto Foi utilizado para meu estudo sobre consumo de APIs.
                     </p>
                     <p>Tecnologias utilizadas - HTML| CSS |JavaScript </p>
@@ -101,7 +106,8 @@ export class ProjectsComponent {
       with: '110px',
       height:'130px',
       
-      description: `<p>Projeto web, onde criei um Gerador de QR code interativo, sendo possível ao usuário digitar um link e a aplicação gera  um QR code correspondente na tela,
+      description: `<img src="assets/img/projects/ProjetoQrCode.png" class="img-custom-size" >
+                    <p>Projeto web, onde criei um Gerador de QR code interativo, sendo possível ao usuário digitar um link e a aplicação gera  um QR code correspondente na tela,
                        também tendo a possibilidade de baixado o QR gerado, como um arquivo de imagem.
                       O projeto Foi utilizado para meu estudo sobre consumo de API e utilização da biblioteca React.
                     </p>
