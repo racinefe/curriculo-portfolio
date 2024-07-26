3f9962e2b2de170ab828a0913085272670113773
@racinefell <102471647+racinefe@users.noreply.github.com>
2024-07-26 12:39:27 -0300

    adicionando gif e img aos projetos

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -20,15 +20,15 @@ export class ProjectsComponent {
   #dialog = inject(MatDialog)
   public arrayProjects = signal<IProjects[]>([
     {
-      src: 'assets/img/projects/ProjetoGeradorSenha.png',
+      src: 'assets/img/projects/ProjetoGeradorSenha/ProjetoGeradorSenha.gif',
       alt: 'Projeto newpass-generator',
       title: 'Newpass-Generator',
-      with: '110px',
-      height:'130px',
+      with: '150px',
+      height:'150px',
       
-      description: `<img src="assets/img/projects/ProjetoGeradorSenha.png" alt='imágem do Projeto newpass-generator' class="img-custom-size" >
-                    <img src="assets/img/projects/ProjetoGeradorSenha2.png" alt='imágem do Projeto newpass-generator' class="img-custom-size" >
-                    <img src="assets/img/projects/ProjetoGeradorSenha3.png" alt='imágem do Projeto newpass-generator' class="img-custom-size" >
+      description: `<img src="assets/img/projects/ProjetoGeradorSenha/ProjetoGeradorSenha.png" alt='imágem do Projeto newpass-generator' class="img-custom-size" >
+                    <img src="assets/img/projects/ProjetoGeradorSenha/ProjetoGeradorSenha2.png" alt='imágem do Projeto newpass-generator' class="img-custom-size" >
+                    <img src="assets/img/projects/ProjetoGeradorSenha/ProjetoGeradorSenha3.png" alt='imágem do Projeto newpass-generator' class="img-custom-size" >
                     <p>
                       Este projeto é uma aplicação web desenvolvida com Angular que inclui um gerador de senhas e uma barra de progresso personalizada.
                        O gerador de senhas permite ao usuário gerar senhas fortes e copiá-las facilmente.
@@ -44,12 +44,15 @@ export class ProjectsComponent {
       ],
     },
     {
-      src: 'assets/img/projects/ProjetoCadastro.png',
+      src: 'assets/img/projects/ProjetoCadastro/ProjetoCadastro.gif',
       alt: 'Projeto Sistema de Matrícula',
       title: `Sistema de Matrícula`,
-      with: '120px',
-      height:'91px',
-      description: `<img src="assets/img/projects/ProjetoCadastro.png" alt='imagem do Projeto Sistema de Matrícula' class="img-custom-size" >
+      with: '200px',
+      height:'130px',
+      description: `<img src="assets/img/projects/ProjetoCadastro/ProjetoCadastro.png" alt='imagem do Projeto Sistema de Matrícula' class="img-custom-size" >
+                    <img src="assets/img/projects/ProjetoCadastro/ProjetoCadastro2.png" alt='imagem do Projeto Sistema de Matrícula' class="img-custom-size" >
+                    <img src="assets/img/projects/ProjetoCadastro/ProjetoCadastro3.png" alt='imagem do Projeto Sistema de Matrícula' class="img-custom-size" >
+                    <img src="assets/img/projects/ProjetoCadastro/ProjetoCadastro5.png" alt='imagem do Projeto Sistema de Matrícula' class="img-custom-size" >
                     <p>Projeto Full Stack de matrícula, utilizando o Firebase como banco de dados para armazenamento,
                        o projeto possui todas as funcionalidades de um GRUD (create, read, update e delete).
                     </p>
@@ -64,12 +67,15 @@ export class ProjectsComponent {
       ],
     },
     {
-      src: 'assets/img/projects/ProjetoToDoList.png',
+      src: 'assets/img/projects/ProjetoToDoList/ProjetoToDoList.gif',
       alt: 'Projeto Lista-de-Tarefas',
       title: 'Lista de Tarefas',
-      with: '120px',
-      height:'91px',
-      description: `<img src="assets/img/projects/ProjetoToDoList.png" alt='imagem do Projeto Projeto Lista-de-Tarefas' class="img-custom-size" >
+      with: '230px',
+      height:'130px',
+      description: `<img src="assets/img/projects/ProjetoToDoList/ProjetoToDoList.png" alt='imagem do Projeto Projeto Lista-de-Tarefas' class="img-custom-size" >
+                    <img src="assets/img/projects/ProjetoToDoList/ProjetoToDoList2.png" alt='imagem do Projeto Projeto Lista-de-Tarefas' class="img-custom-size" >
+                    <img src="assets/img/projects/ProjetoToDoList/ProjetoToDoList3.png" alt='imagem do Projeto Projeto Lista-de-Tarefas' class="img-custom-size" >
+                    <img src="assets/img/projects/ProjetoToDoList/ProjetoToDoList6.png" alt='imagem do Projeto Projeto Lista-de-Tarefas' class="img-custom-size" >
                     <p>Projeto web, onde criei um to-do list, o projeto possui todas as funcionalidades do GRUD (create, read, update e delete),
                        além de um checklist interativo.
                     </p>
@@ -83,34 +89,17 @@ export class ProjectsComponent {
       ],
     },
     {
-      src: 'assets/img/projects/ProjetoMentor.png',
-      alt: 'Projeto Newsletter sign-up',
-      title: 'Newsletter sign-up',
-      with: '120px',
-      height:'91px',
-      
-      description: `<img src="assets/img/projects/ProjetoMentor.png" alt='imagem do Projeto Newsletter sign-up' class="img-custom-size" >
-                    <p>Projeto web, onde cumpri o desafio de construir um formulário de boletim informativo e fazê-lo parecer o mais próximo possível do design.</p>
-                    <p>O desafio foi retirado do Frontend Mentor: <a href="https://www.frontendmentor.io/challenges/newsletter-signup-form-with-success-message-3FC1AZbNrv" target="_blank" class="btn btn-primary">Pagina do desafio</a></p>
-                    <p>Tecnologias utilizadas - HTML| CSS |JavaScript | React | vite</p>
-                   `,
-      links: [
-        {
-          name: 'Conheça o Projeto',
-          href: 'https://pjnewslettersign-up.netlify.app/',
-                      
-        },
-        
-      ],
-    },
-    {
-      src: 'assets/img/projects/ProjetoPokedex.png',
+      src: 'assets/img/projects/ProjetoPokedex/ProjetoPokedex.gif',
       alt: 'Projeto Pokédex',
       title: 'Pokédex',
-      with: '120px',
-      height:'91px',
+      with: '200px',
+      height:'130px',
       
-      description: `<img src="assets/img/projects/ProjetoPokedex.png" alt='imagem do Projeto Pokédex' class="img-custom-size" >
+      description: `<img src="assets/img/projects/ProjetoPokedex/ProjetoPokedex1.png" alt='imagem do Projeto Pokédex' class="img-custom-size" >
+                    <img src="assets/img/projects/ProjetoPokedex/ProjetoPokedex2.png" alt='imagem do Projeto Pokédex' class="img-custom-size" >
+                    <img src="assets/img/projects/ProjetoPokedex/ProjetoPokedex3.png" alt='imagem do Projeto Pokédex' class="img-custom-size" >
+                    <img src="assets/img/projects/ProjetoPokedex/ProjetoPokedex4.png" alt='imagem do Projeto Pokédex' class="img-custom-size" >
+                    <img src="assets/img/projects/ProjetoPokedex/ProjetoPokedex7.png" alt='imagem do Projeto Pokédex' class="img-custom-size" >
                     <p>Projeto web, onde criei um Pokédex interativo sendo possível pesquisar através do nome de um Pokémon ou do ID.
                        O projeto Foi utilizado para meu estudo sobre consumo de APIs.
                     </p>
@@ -124,13 +113,42 @@ export class ProjectsComponent {
       ],
     },
     {
-      src: 'assets/img/projects/ProjetoQrCode.png',
-      alt: 'Projeto Gerador de QrCode',
-      title: 'Gerador de QrCode',
-      with: '110px',
+      src: 'assets/img/projects/ProjetoMentor/ProjetoMentor.gif',
+      alt: 'Projeto Newsletter sign-up',
+      title: 'Newsletter sign-up',
+      with: '200px',
       height:'130px',
       
-      description: `<img src="assets/img/projects/ProjetoQrCode.png"  alt='imagem do Projeto Gerador de QrCode' class="img-custom-size" >
+      description: `<img src="assets/img/projects/ProjetoMentor/ProjetoMentor1.png" alt='imagem do Projeto Newsletter sign-up' class="img-custom-size" >
+                    <img src="assets/img/projects/ProjetoMentor/ProjetoMentor2.png" alt='imagem do Projeto Newsletter sign-up' class="img-custom-size" >
+                    <img src="assets/img/projects/ProjetoMentor/ProjetoMentor3.png" alt='imagem do Projeto Newsletter sign-up' class="img-custom-size" >
+
+                    <p>Projeto web, onde cumpri o desafio de construir um formulário de boletim informativo e fazê-lo parecer o mais próximo possível do design.</p>
+                    <p>O desafio foi retirado do Frontend Mentor: <a href="https://www.frontendmentor.io/challenges/newsletter-signup-form-with-success-message-3FC1AZbNrv" target="_blank" class="btn btn-primary">Pagina do desafio</a></p>
+                    <p>Tecnologias utilizadas - HTML| CSS |JavaScript | React | vite</p>
+                   `,
+      links: [
+        {
+          name: 'Conheça o Projeto',
+          href: 'https://pjnewslettersign-up.netlify.app/',
+                      
+        },
+        
+      ],
+    },
+    
+    {
+      src: 'assets/img/projects/ProjetoQrCode/ProjetoQrCode.gif',
+      alt: 'Projeto Gerador de QrCode',
+      title: 'Gerador de QrCode',
+      with: '200px',
+      height:'130px',
+      
+      description: `<img src="assets/img/projects/ProjetoQrCode/ProjetoQrCode2.png"  alt='imagem do Projeto Gerador de QrCode' class="img-custom-size" >
+                    <img src="assets/img/projects/ProjetoQrCode/ProjetoQrCode3.png"  alt='imagem do Projeto Gerador de QrCode' class="img-custom-size" >
+                    <img src="assets/img/projects/ProjetoQrCode/ProjetoQrCode5.png"  alt='imagem do Projeto Gerador de QrCode' class="img-custom-size" >
+                    <img src="assets/img/projects/ProjetoQrCode/ProjetoQrCode6.png"  alt='imagem do Projeto Gerador de QrCode' class="img-custom-size" >
+                    
                     <p>Projeto web, onde criei um Gerador de QR code interativo, sendo possível ao usuário digitar um link e a aplicação gera  um QR code correspondente na tela,
                        também tendo a possibilidade de baixado o QR gerado, como um arquivo de imagem.
                       O projeto Foi utilizado para meu estudo sobre consumo de API e utilização da biblioteca React.
