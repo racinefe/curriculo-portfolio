470694caca36df34c9069d738b9f4f7feb4c9a0b
@racinefell <102471647+racinefe@users.noreply.github.com>
2024-11-15 11:17:13 -0300

    alterandp path da home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,6 @@ import { HomeComponent } from './modules/portfolio/pages/home/home.component';
 import { BlogComponent } from './blog/blog.component';
 
 export const routes: Routes = [
-    { path:'',component: HomeComponent },
+    { path: '', component: HomeComponent, pathMatch: 'full' },
     { path: 'blog', component: BlogComponent },
 ];
